e8e6ec1ea11d65eaa822bc8794ada8a8ba776f13
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2023-06-29 16:26:26 -0300

    Initial Commit

---
+++ Aula GitHub/Aula_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_GitHubApp.swift
+//  Aula GitHub
+//
+//  Created by Thiago Liporace on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
